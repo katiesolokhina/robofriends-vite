c644c5fb1334aa3d3acefa98ea74720d1a810f9e
katiesolokhina <47326861+katiesolokhina@users.noreply.github.com>
2025-09-22 01:41:32 -0400

    Initial Commit

---
+++ src/containers/App.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import CardList from "../components/CardList.jsx";
+import SearchBox from "../components/SearchBox.jsx";
+import Scroll from "../components/Scroll.jsx";
+import ErrorBoundry from "../components/ErrorBoundry.jsx";
+import "./App.css";
+
+function App() {
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState("");
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then(users => {setRobots(users)});
+  },[]);
+
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value);
+  };
+
+  const filteredRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  });
+  return !robots.length ? (
+    <h1>Loading</h1>
+  ) : (
+    <div className="tc">
+      <h1 className="f1">RoboFriends</h1>
+      <SearchBox searchChange={onSearchChange} />
+      <Scroll>
+        <ErrorBoundry>
+          <CardList robots={filteredRobots} />
+        </ErrorBoundry>
+      </Scroll>
+    </div>
+  );
+}
+
+export default App;
